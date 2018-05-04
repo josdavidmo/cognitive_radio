815dd1a4f5d4ccfa7d2ac8ebd62ff5fc8cd68f23
pabarragang@outlook.es <pabarragang@outlook.es>
2018-05-04 06:39:54 -0500

    SVM

--- anfis_code.m
+++ anfis_code.m
@@ -1,14 +1,20 @@
 NumMf = 3;
 MfType = str2mat('pimf','pimf','pimf','pimf','trimf');
 results = [];
-numEpochs=100;
-parfor i = 1:9
-    start = (110200*(i-1))+1
-    fin = i*110200
-    data = result_wifi(start:fin,:);
+numEpochs=50;
+train_data = result_wifi(1:27550,:);
+
+if isempty(gcp('nocreate'))
+    parpool('local', 2)
+end
+
+parfor i = 1:5
+    start = (5510*(i-1))+1
+    fin = i*5510
+    data = train_data(start:fin,:);
     fismat = genfis1(data, NumMf, MfType);
-    trndata = data(1:77140,:);
-    chkdata = data(77141:110200 ,:);
+    trndata = data(1:3857,:);
+    chkdata = data(3858:5510,:);
     [fismat1,trnErr,ss,fismat2,chkErr]=anfis(trndata,fismat,numEpochs,NaN,chkdata);
     
     %results(i) = [fismat1,trnErr,ss,fismat2,chkErr];

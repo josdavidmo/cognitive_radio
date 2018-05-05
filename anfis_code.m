2d20bb5992440eb167fc1a8010d8eb1c5d639524
pabarragang@outlook.es <pabarragang@outlook.es>
2018-05-05 17:43:56 -0500

    ANFIS code

--- anfis_code.m
+++ anfis_code.m
@@ -1,12 +1,17 @@
 NumMf = 3;
-MfType = str2mat('pimf','pimf','pimf','pimf','trimf');
+MfType = str2mat('pimf','pimf','pimf','trimf');
 results = [];
-numEpochs=50;
-train_data = result_wifi(1:27550,:);
+numEpochs=20;
+currently_data= result_wifi(1:27550,:);
+colmin = min(currently_data);
+colmax = max(currently_data);
+train_data = rescale(currently_data,'InputMin',colmin,'InputMax',colmax);
 
 if isempty(gcp('nocreate'))
     parpool('local', 2)
 end
+
+fid = fopen('log.txt','wt');
 
 parfor i = 1:5
     start = (5510*(i-1))+1
@@ -14,10 +19,13 @@ parfor i = 1:5
     data = train_data(start:fin,:);
     fismat = genfis1(data, NumMf, MfType);
     trndata = data(1:3857,:);
+    
     chkdata = data(3858:5510,:);
     [fismat1,trnErr,ss,fismat2,chkErr]=anfis(trndata,fismat,numEpochs,NaN,chkdata);
-    
+    fprintf(fid, 'ITERACION %d START %d LAST %d ERROR %d', i, start, fin, trnErr);
     %results(i) = [fismat1,trnErr,ss,fismat2,chkErr];
     results(i,:) = [start,fin]
 end
 
+delete(gcp('nocreate'));
+fclose(fid);
--- svm_code.m
+++ svm_code.m
@@ -1,20 +1,11 @@
-file_data = csvread('data.csv');
+file_data = result_wifi;
 labels = file_data(:,5);
 data = sparse(file_data(:,1:4));
-train_data = data(1:5000,:);
-train_label = labels(1:5000,:);
-
-if isempty(gcp('nocreate'))
-    parpool('local', 2)
+train_data = data(1:40000,:);
+train_label = labels(1:40000,:);
+parfor i = 1:8
+    start = (5000*(i-1))+1;
+    last = i*5000;
+    model = svmtrain(train_label(start:last,:), train_data(start:last,:), '-t 0');
 end
-
-idx = 1:500:length(train_data);
-parfor (block = 1:numel(idx))
-    start = block(1);
-    last = block(2);
-    block_data = train_data(start:last,:);
-    block_label = train_label(start:last,:);
-    model_linear = fitcsvm(block_label, block_data, '-t 0');
-end
-delete(gcp('nocreate'))
-libsvmwrite('data.txt', labels, data);
+delete(gcp('nocreate'));

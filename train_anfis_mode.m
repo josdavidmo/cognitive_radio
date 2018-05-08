f4b7ec7448a000b0f4432fd3967388f3f637a98c
Andrea Barragan <andreabarragan@ticsocial.com.co>
2018-05-08 09:39:01 -0500

    ANFIS Function

--- train_anfis_mode.m
+++ train_anfis_mode.m
@@ -1,22 +1,22 @@
-function [fis_mat,index] = train_anfis_mode(currently_data,NumMf,MfType,numEpochs,towers,records)
+function [fis_mat,start_a,last_a,trn_err,chk_err] = train_anfis_mode(currently_data,NumMf,MfType,numEpochs,towers)
     colmin = min(currently_data);
     colmax = max(currently_data);
     train_data = rescale(currently_data,'InputMin',colmin,'InputMax',colmax);
-    data_size = size(file_data);
-    batch_size = floor(data_size(1)/num_towers);
-    index = [];
+    data_size = size(train_data);
+    batch_size = floor(data_size(1)/towers);
     parfor i = 1:towers
-        start = (records*(i-1))+1;
+        start = (batch_size*(i-1))+1;
         last = min([i*batch_size data_size(1)]);
         data = train_data(start:last,:);
         fismat = genfis1(data, NumMf, MfType);
-        trndata = data(1:records*0.7,:);        
-        chkdata = data(records*0.7+1:records,:);
+        trndata = data(1:last*0.7,:);        
+        chkdata = data(last*0.7+1:last,:);
         [fismat1,trnErr,ss,fismat2,chkErr]=anfis(trndata,fismat,numEpochs,NaN,chkdata);
         fis_mat(i) = fismat1;
-        row = [i,start,last,trnErr,chkErr];
-        index = [index;row];
-        fprintf('ITERACION %d START %d LAST %d ERROR %d \n', i, start, last, trnErr);
+        start_a(i) = start;
+        last_a(i) = last;
+        trn_err(i) = trnErr;
+        chk_err(i) = chkErr;
     end
     delete(gcp('nocreate'));
 end

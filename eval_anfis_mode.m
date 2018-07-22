75bb2c138bbf7d186ad97f26cf2af4320676f578
josdavidmo <pabarragang@outlook.es>
2018-07-21 20:46:56 -0500

    Export results functionality

--- eval_anfis_mode.m
+++ eval_anfis_mode.m
@@ -2,10 +2,12 @@ function [accuracy] = eval_anfis_mode(train_data,fis)
     [rows,columns] = size(train_data);
     accuracy = 0;
     for i=1:rows
-        output = evalfis(train_data(i,:),fis);        
+        output = evalfis(train_data(i,:),fis);  
+        matrix_anfis(i,:) = [train_data(i,:),output]
         if round(output)>=1
             accuracy = accuracy + 1;
         end
     end
+    csvwrite('results_svm.csv',matrix_anfis);
     accuracy = accuracy/rows
 end
--- train_svm_model.m
+++ train_svm_model.m
@@ -8,17 +8,24 @@ function [towers, accuracy, error] = train_svm_model(file_data, num_towers, opti
     data_size = size(file_data);
     batch_size = floor(data_size(1)/num_towers);
     hbar = parfor_progressbar(num_towers,'Training Models');
-    parfor i = 1:num_towers
+    parfor i = 1:num_towers     
         start = (batch_size*(i-1))+1;
         last = min([i*batch_size data_size(1)]);
         fprintf('Training data %d - %d\n', start, last);
         model = svmtrain(train_label(start:last,:), train_data(start:last,:), options);
-        towers(i) = model;
-        [~, result, ~] = svmpredict(train_label(start:last,:), train_data(start:last,:), model);
-        accuracy(i) = result(1);
-        error(i) = result(2);
+        towers(i) = model;        
         hbar.iterate(1);
     end
+    for i = 1:num_towers
+        start = (batch_size*(i-1))+1;
+        last = min([i*batch_size data_size(1)]);
+        [lbl, result, ~] = svmpredict(train_label(start:last,:), train_data(start:last,:), towers(i));
+        accuracy(i) = result(1);
+        error(i) = result(2);
+        fprintf('Results');
+        matrix_svm(start:last,:) = [file_data(start:last,1:4),lbl]
+    end
+    csvwrite('results_svm.csv',matrix_svm);
     close(hbar);
     delete(gcp('nocreate'))
 end
